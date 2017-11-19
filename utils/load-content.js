b981e995f18fa7310c6844d6e4bf4cdcd0c27093
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2017-11-19 04:01:51 -0600

    backup

--- utils/load-content.js
+++ utils/load-content.js
@@ -10,7 +10,60 @@ const Project = mongoose.model('Project');
 const dataDir = '../data/project-pages/';
 const files = fs.readdirSync(dataDir);
 
-const processFile = function(file, callback) {
+
+
+
+//readContent('kronofoto.html', updateProject);
+main();
+
+function main() {
+  async.each(files, readContent, function onDone(err) {
+    if (err) throw err;
+    console.log('done, disconnecting');
+    //mongoose.disconnect();
+  });
+};
+
+
+
+function readContent(filename, cb) {
+  cb();
+  console.log('called on ' + filename);
+  let projectId = path.basename(filename, '.html');
+  let filePath = path.resolve(dataDir, filename);
+
+  fs.readFile(filePath, 'utf8', (err, content) => {
+    if (err) throw err;
+    cb(projectId, content, logResult);
+  });
+};
+
+function updateProject(projectId, data, cb) {
+  console.log('called');
+  Project.update({_id: projectId}, {content: data}, (err, dbResponse) => {
+    if (err) throw err;
+    cb(dbResponse);
+  });
+};
+
+function logResult(message) {
+  console.log(message);
+  //mongoose.disconnect();
+};
+
+
+
+
+
+
+
+
+
+
+
+//const processFile = function(file, callback) {
+
+function processFile(file, callback) {
   let projectId = path.basename(file, '.html');
   let dataFile = path.resolve(dataDir, file);
 
@@ -25,7 +78,7 @@ const processFile = function(file, callback) {
   });
 };
 
-async.each(files, processFile, (err) => {
-  if (err) throw err;
-  mongoose.disconnect(); //TODO doesn't look right: why for each file?
-});
+//async.each(files, processFile, (err) => {
+//  if (err) throw err;
+//  mongoose.disconnect(); //TODO doesn't look right: why for each file?
+//});
